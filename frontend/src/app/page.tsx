662b8cabb349bada559a8a83605705a95759d569
MingPV <106954833+MingPV@users.noreply.github.com>
2025-06-30 17:20:34 +0700

    change tailwind version

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,27 +7,38 @@ export default function Page() {
   useEffect(() => {}, []);
 
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center">
-      <div className="text-8xl font-bold text-stone-400 mb-10">GameName.io</div>
-      <div className=" flex flex-col gap-4 mb-24 items-center">
-        <Link
-          href={"/sign-in"}
-          className=" font-bold text-xl hover:bg-stone-700 px-4 py-2 text-stone-300 transition-all duration-200"
-        >
-          Play game
-        </Link>
-        <Link
-          href={"/sign-up"}
-          className=" font-bold text-xl hover:bg-stone-700 px-4 py-2 text-stone-300 transition-all duration-200"
-        >
-          Create an account
-        </Link>
-        <Link
-          href={"/sign-in"}
-          className=" font-bold text-xl hover:bg-stone-700 px-4 py-2 text-stone-300 transition-all duration-200"
-        >
-          Settings
-        </Link>
+    <div
+      className="w-full h-full flex flex-col justify-center items-center"
+      style={{
+        backgroundImage: "url('/images/game3.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <div className="absolute inset-0 bg-black opacity-50 z-0" />
+      <div className="flex flex-col justify-center items-center z-10">
+        <div className="text-8xl text-stone-400 mb-10 font-gameFont">Type</div>
+        <div className=" flex flex-col gap-4 mb-24 items-center">
+          <Link
+            href={"/sign-in"}
+            className=" font-bold text-xl hover:bg-stone-700 px-4 py-2 text-stone-300 transition-all duration-200 "
+          >
+            Play game
+          </Link>
+          <Link
+            href={"/sign-up"}
+            className=" font-bold text-xl hover:bg-stone-700 px-4 py-2 text-stone-300 transition-all duration-200 "
+          >
+            Create an account
+          </Link>
+          <Link
+            href={"/sign-in"}
+            className=" font-bold text-xl hover:bg-stone-700 px-4 py-2 text-stone-300 transition-all duration-200 "
+          >
+            Settings
+          </Link>
+        </div>
       </div>
     </div>
   );
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        gameFont: ["GameFont"],
+      },
+      colors: {
+        brandBlue: "#1DA1F2",
+        softPink: "#FFB6C1",
+        customGreen: {
+          light: "#6EE7B7",
+          DEFAULT: "#10B981",
+          dark: "#047857",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
